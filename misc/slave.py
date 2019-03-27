af70e18b7edbd14f0e05ebc337e533d350458abe
Morgan Webb <maw1862@rit.edu>
2019-03-27 08:57:44 -0400

    Fixed path issue in the slave script

--- misc/slave.py
+++ misc/slave.py
@@ -14,7 +14,7 @@ locksPath = os.path.join(home, 'tmp')
 triggerFile = os.path.join(locksPath, 'trigger_cameras.lock')
 
 # Create a path to the shared image directory
-imagesPath = os.path.join(home, 'images')
+imagesPath = os.path.join(home, 'data')
 
 # Create the capture command
 capture = os.path.join(home, 'src')
